984d9342a47395a06ab7971c4b42ac24d4d45195
Микрюков Григорий <mikryukov@ecredit.one>
2024-03-04 21:40:18 +0400

    add bucket

--- terraform/stage/main.tf
+++ terraform/stage/main.tf
@@ -8,6 +8,7 @@ module "app" {
   source          = "../modules/app"
   public_key_path = var.public_key_path
   app_disk_image  = var.app_disk_image
+  private_key_path= var.private_key_path
   subnet_id       = var.subnet_id
 }
 
@@ -15,6 +16,7 @@ module "db" {
   source          = "../modules/db"
   public_key_path = var.public_key_path
   db_disk_image   = var.db_disk_image
+  private_key_path= var.private_key_path
   subnet_id       = var.subnet_id
 }
 
