a37eb73ebebc896343695db66b395666afcaafec
Микрюков Григорий <mikryukov@ecredit.one>
2024-03-04 22:09:43 +0400

    add bucket

--- terraform/main.tf
+++ terraform/main.tf
@@ -4,6 +4,23 @@ provider "yandex" {
   zone                     = var.yc_region
   service_account_key_file = var.yc_service_account_key_file
 }
+terraform {
+    backend "s3" {
+    endpoint = "https://storage.yandexcloud.net"
+    bucket = "backend-terraform-gmikryukov"
+    region = "ru-central1-a"
+    key    = "../../key.json"
+    access_key = yandex_iam_service_account_static_access_key.sa-static-key.access_key
+    secret_key = yandex_iam_service_account_static_access_key.sa-static-key.secret_key
+
+
+    skip_region_validation      = true
+    skip_credentials_validation = true
+    # skip_requesting_account_id  = true # Необходимая опция Terraform для версии 1.6.1 и старше.
+    # skip_s3_checksum            = true # Необходимая опция при описании бэкенда для Terraform версии 1.6.3 и старше.
+
+  }
+}
 module "app" {
   source          = "./modules/app"
   public_key_path = var.public_key_path
--- terraform/storage-bucket.tf
+++ terraform/storage-bucket.tf
@@ -1,4 +1,23 @@
+resource "yandex_iam_service_account" "sa" {
+  name = "backend"
+}
 
-resource "yandex_storage_bucket" "backend" {
-  bucket     = "backend.tf"
+// Назначение роли сервисному аккаунту
+resource "yandex_resourcemanager_folder_iam_member" "sa-editor" {
+  folder_id = var.yc_folder_id
+  role      = "storage.editor"
+  member    = "serviceAccount:${yandex_iam_service_account.sa.id}"
+}
+
+// Создание статического ключа доступа
+resource "yandex_iam_service_account_static_access_key" "sa-static-key" {
+  service_account_id = yandex_iam_service_account.sa.id
+  description        = "static access key for object storage"
+}
+
+// Создание бакета с использованием ключа
+resource "yandex_storage_bucket" "gmikryukov" {
+  access_key = yandex_iam_service_account_static_access_key.sa-static-key.access_key
+  secret_key = yandex_iam_service_account_static_access_key.sa-static-key.secret_key
+  bucket     = "backend-terraform-gmikryukov"
 }
