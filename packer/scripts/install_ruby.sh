6d9d193b8f2d719b59b9eb35c881b71a83d79e05
Микрюков Григорий <mikryukov@ecredit.one>
2024-03-09 09:42:33 +0400

    fix packer images

--- packer/scripts/install_ruby.sh
+++ packer/scripts/install_ruby.sh
@@ -1,4 +1,5 @@
 #!/bin/sh
 sleep 20
-apt-get update && \
-apt-get install -y ruby-full ruby-bundler build-essential
+
+sudo add-apt-repository -y ppa:jblgf0/python && apt-get update && apt-get install -y python3.6 ruby-full ruby-bundler build-essential
+sudo update-alternatives --install /usr/bin/python3 python3 /usr/bin/python3.6 2
